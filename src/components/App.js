ba082b08754af433ed54bbde40c9ee306b50eb10
shiba_1234 <shibajisahu121@gmail.com>
2023-09-06 02:42:21 +0530

    initial comit and Navbar designed

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import NavBar from "./NavBar";
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      <h1>tried</h1>
+    </div>
+  );
+}
+
+export default App;
